a0a625f6b0ffae3eee47f35dfd4c6714e1babdbd
Calebk2024 <calebkimjk@gmail.com>
2023-12-01 12:55:22 -0800

    Files initialized

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, send_file, redirect, request, jsonify, render_template
-from PIL import Image
+#from PIL import Image
 app = Flask(__name__)
+@app.route("/")
+def r():
+    return send_file("index.html")
+
 
 if __name__ == "__main__":
     """
     Sets the port to 4242 for incoming HTTP requests.
     """
-    app.run(port=4242)
+    app.run(port=4241)
